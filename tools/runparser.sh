aba1cef0ec13c40ecf76bb4c6c5fe18b2c6552e0
Roger Luethi <rl@patchworkscience.org>
2016-12-29 18:15:41 +0100

    Robustness fixes in runparser
    This patch contains a few improvements to runparser which should make it
    more robust and user-friendly.
    
    The script...
      ... now aborts if an error occurs instead of happily claiming that it
          succeeded
      ... does not try to clone the repo if it is already there
      ... no longer insists on using Python 2.7
      ... can work on systems that don't have bash in /bin
      ... prints a more concise, less verbose message
    
    The first three changes fix buggy behavior observed in testing.
    
    Change-Id: I289d3313ed57670e51783cb2c80f799971889e07

--- tools/runparser.sh
+++ tools/runparser.sh
@@ -1,14 +1,32 @@
-#!/bin/bash -
+#!/usr/bin/env bash
+set -o errexit -o nounset
 
-mkdir -p build/scripts
-git clone \
-    --depth 10 \
-    git://git.openstack.org/openstack/openstack-manuals \
-    build/openstack-manuals
-git clone \
-    git://git.openstack.org/openstack/training-labs \
-    build/training-labs
+# TODO: OUTPUT_DIR should either be read from the config file used by
+#       parser.py or passed to parser.py (argument or environment variable);
+#       every path should only be configured in one location
+OUTPUT_DIR=build/scripts
 
-python2.7 rst2bash/parser.py
+mkdir -p "$OUTPUT_DIR"
 
-echo "Please find the generated BASH scripts in the build/scripts folder!".
+REPO=openstack-manuals
+# Only clone the repo if it does not exist yet. We could update an existing
+# repo here, but skipping the repo instead allows for easy offline testing.
+if [ ! -e build/$REPO ]; then
+    git clone \
+        --depth 10 \
+        git://git.openstack.org/openstack/openstack-manuals \
+        build/$REPO
+fi
+
+REPO=training-labs
+if [ ! -e build/$REPO ]; then
+    git clone \
+        git://git.openstack.org/openstack/training-labs \
+        build/$REPO
+fi
+
+# Aim at writing portable code that works with Python 2.x and (hopefully)
+# with Python 3 as well.
+python rst2bash/parser.py
+
+echo "Output written to $OUTPUT_DIR"
